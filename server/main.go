74f849d705e2e8bf064ca3c5221840215f3e8d29
Nandha-Kishore-05 <151624744+Nandha-Kishore-05@users.noreply.github.com>
2024-09-27 10:32:42 +0530

    cors added

--- server/main.go
+++ server/main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"CARBON_CALCULATOR/config" // Import the generated Swagger docs
-
 	"CARBON_CALCULATOR/routes"
 
+	cors "github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
@@ -21,6 +21,18 @@ func main() {
 
 	// Initialize Gin router
 	router := gin.Default()
+
+	// CORS configuration
+	corsConfig := cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"}, // Allow only this origin
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}
+
+	// Apply CORS middleware
+	router.Use(cors.New(corsConfig))
 
 	// Register routes
 	routes.RegisterRoutes(router)
