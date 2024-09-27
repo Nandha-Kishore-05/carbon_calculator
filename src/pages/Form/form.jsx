3bbecccd7824559dfa77cb44e0021c5d5ced5336
Nandha-Kishore-05 <151624744+Nandha-Kishore-05@users.noreply.github.com>
2024-09-27 08:36:46 +0530

    resolved conflict

--- src/pages/Form/form.jsx
+++ src/pages/Form/form.jsx
@@ -1,30 +1,24 @@
-import React from "react";
-import sun from '../../assets/sun.png';
-import tree from '../../assets/Trees@2x.png';
-import './form.css';
+import  Typography  from '@mui/material/Typography'
+import React from 'react'
+import CustomButton from '../../components/button/button'
+import {useNavigate} from "react-router-dom"
 
-function FormSummition() {
-   
+function Form() {
+    const navigate = useNavigate();
+  const handleClick =() =>{
+   navigate('/form');
+  }
   return (
-    <div className='first'>
-        <div className='head'></div>
-        <img className='sun-cloud' src={sun} alt="Sun and clouds" />
-        <div className='middle'></div>
-        <div class="text-animation">
-            Great job! You're making a positive <br/>contribution to preserving our <br/>green environment.
-        </div>
-        <div class="form-animation">
+    <Typography variant='h5'>
+      registration form...
+      <CustomButton
+                onClick={handleClick}
+                width={150}
+                label="Apply Leave"
             
-            <div className="form-data">
-            <form>
-              <input type="text" placeholder="Enter your name" />
-              <input type="submit" value="Submit" />
-            </form>
-            </div>
-        </div>
-        <img className='tree' src={tree} alt="Sun and clouds" />
-    </div>
-  );
-
+              />
+    </Typography>
+  )
 }
-export default FormSummition
+
+export default Form
